f208bf4bab74d33c0e9a545703b0f18215e58e84
krish-kakadiya <krishkakadiya000007@gmail.com>
2025-10-09 17:30:15 +0530

    feat: Add Calculator component with basic arithmetic functionality and styling

--- fsd-classwork/src/App.jsx
+++ fsd-classwork/src/App.jsx
@@ -3,6 +3,7 @@ import VotingSystem from './Practical/Practical1/VotingSystem'
 import WeatherApp from './Practical/Practical2/WeatherApp'
 import WelcomeClock from './Practical/Practical3/WelcomeClock'
 import CounterApp from './Practical/Practical4/CounterApp'
+import Calculator from './Practical/Practical5/Calculator'
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <>
-      <CounterApp/>
+      <Calculator/>
     </>
   )
 }
